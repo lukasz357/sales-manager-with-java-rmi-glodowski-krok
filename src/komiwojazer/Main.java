42c3332f0672d2724742237294d52ad41c5f8788
krzysztof.glodowski <krzysztof.glodowski@gmail.com>
2012-06-05 19:15:16 +0000

    zmiana na MapUtil

--- src/komiwojazer/Main.java
+++ src/komiwojazer/Main.java
@@ -26,8 +26,8 @@ public class Main {
 
     public static void main(String[] args) throws BadArgsException, ClassNotFoundException, SQLException, IOException, FileFormatException {
 
-    	CityMap m = readMap("graph.txt");
-        Worker or = new Worker(m);
+    	
+        MapUtil or = new MapUtil(m);
         // go(liczba_pokolen, liczebnosc_pokolenia
         ArrayList<City> result = or.go(1000, 100);
         StringBuffer sb = new StringBuffer();
@@ -41,44 +41,6 @@ public class Main {
     
     
     
-    private static CityMap readMap(String path) throws IOException {
-    	TreeMap<String, City> nodes = new TreeMap<String, City>();
-        String tmp; String [] line; int fileLine=1;
-
-        BufferedReader in = new BufferedReader(new FileReader(new File(path)));
-        //sprawdzam czy jest nagłówek Nodes:
-        if(!(tmp = in.readLine()).equals("Nodes:")){
-            throw new FileFormatException(1, "Brak naglowka \"Nodes:\"");
-        }
-
-        //wczytuję wierzchołki grafu
-        while((tmp = in.readLine())!=null && !tmp.equals("Edges:") ){
-            nodes.put(tmp, new City(tmp));
-            fileLine++;
-        }
-
-        //sprawdzam czy jest nagłówek Edges:
-        if(tmp==null){
-            throw new FileFormatException(fileLine, "Brak naglowka \"Edges:\"");
-        }else{
-            //wczytuję połaczenia między wierzchołkami
-            while((tmp = in.readLine()) != null){
-                ++fileLine;
-                line=tmp.split(" ");
-                if(line.length!=4)
-                    throw new FileFormatException(fileLine, "Bledna ilosc danych. Wymagane 4.");
-                nodes.get(line[0]).AddOutgoingRoute(nodes.get(line[1]), Integer.parseInt(line[2]),Integer.parseInt(line[3]));
-            }
-        }
-        in.close();
-
-        return new CityMap(nodes);
-    }
     
-    static class FileFormatException extends IOException{
-        public FileFormatException(int line, String message){
-            super("Blad struktury pliku w linii " + line + ": " + message);
-        }
-    }
     
 }
