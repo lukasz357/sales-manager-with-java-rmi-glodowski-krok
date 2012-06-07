6d3b104cfa3b8f5b693f453780b5045b59d8f5b0
krzysztof.glodowski <krzysztof.glodowski@gmail.com>
2012-06-07 20:50:06 +0000

    bububu

--- src/komiwojazer/rmi/Server.java
+++ src/komiwojazer/rmi/Server.java
@@ -13,7 +13,7 @@ import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Stack;
 import java.util.TreeMap;
 
 import komiwojazer.MapUtil;
@@ -22,19 +22,30 @@ import komiwojazer.GeneticAlgorithm.Path;
 import komiwojazer.Map.City;
 import komiwojazer.Map.CityMap;
 
-public class Server extends UnicastRemoteObject implements ServerInterface{
+public class Server extends UnicastRemoteObject implements ServerInterface {
 
 	private static final long serialVersionUID = -2065432534190043743L;
-	
+
 	private float crossProb, mutateProb;
 	private int generationsCount, populationCount;
-	
+
 	private Costs costArray;
 	private int[] nodeNumbers;
-	
+
 	private CityMap map;
-	
+	private MapUtil mu;
+
 	private List<Integer> clientRegistry;
+	private List<Integer> clientQueue;
+
+	private Path actualBest;
+	private List<Path> currentGeneration;
+
+
+	private Object lock;
+	private Object notifier;
+
+	private Stack<List<Path>> clientResults;
 
 	public Server() throws RemoteException {
 		super();
@@ -50,18 +61,28 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 		super(port);
 		init();
 	}
-	
-	private void init(){
+
+	private void init() {
+		clientResults = new Stack<List<Path>>();
+		lock = new Object();
+		notifier = new Object();
 		clientRegistry = new ArrayList<Integer>();
+		mu = new MapUtil(map);
+		costArray = mu.getCostArray();
+		nodeNumbers = mu.getNodeNumbers();
+		currentGeneration = new ArrayList<Path>();
+		for (int i = 0; i < populationCount; i++) {
+			currentGeneration.add(new Path(nodeNumbers.length).random());
+		}
+		new Session().start();
 	}
-	
 
 	@Override
 	public float getMutationProbability() throws RemoteException {
 		return mutateProb;
 	}
-	
-	public void setMutationProbability(float prob){
+
+	public void setMutationProbability(float prob) {
 		this.mutateProb = prob;
 	}
 
@@ -69,7 +90,8 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 	public float getCrossProbability() throws RemoteException {
 		return crossProb;
 	}
-	public void setCrossProbability(float prob){
+
+	public void setCrossProbability(float prob) {
 		crossProb = prob;
 	}
 
@@ -77,7 +99,8 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 	public int getPopulationCount() throws RemoteException {
 		return populationCount;
 	}
-	public void setPopulationCount(int count){
+
+	public void setPopulationCount(int count) {
 		populationCount = count;
 	}
 
@@ -85,8 +108,8 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 	public int getGenerationsCount() throws RemoteException {
 		return generationsCount;
 	}
-	
-	public void setGenerationsCount(int count){
+
+	public void setGenerationsCount(int count) {
 		generationsCount = count;
 	}
 
@@ -94,8 +117,8 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 	public Costs getCostArray() throws RemoteException {
 		return costArray;
 	}
-	
-	public void setCostArray(Costs c){
+
+	public void setCostArray(Costs c) {
 		costArray = c;
 	}
 
@@ -103,28 +126,108 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 	public int[] getNodeNumbers() throws RemoteException {
 		return nodeNumbers;
 	}
-	
-	public void setNodeNumbers(int[] num){
+
+	public void setNodeNumbers(int[] num) {
 		nodeNumbers = num;
 	}
-	
-	public void setMap(CityMap m){
+
+	public void setMap(CityMap m) {
 		map = m;
 	}
 
 	@Override
 	public List<Path> register(int clientID) throws RemoteException {
-		return null;
+		// klient zapisuje sie do kolejki
+		clientQueue.add(clientID);
+		try {
+			if(clientRegistry.size() == 0){
+				//jesli jest pierwszy to obudz watek
+				synchronized (notifier) {
+					notifier.notify();
+				}
+			}
+			// i czeka
+			synchronized (lock) {
+				lock.wait();
+			}
+			// dostal pozwolenie to teraz zapisuje sie
+			// i w zamian dostaje pokolenie
+			clientQueue.remove(new Integer(clientID));
+			clientRegistry.add(clientID);
+			return currentGeneration;
+		} catch (InterruptedException e) {
+			return null;
+		}
 	}
 
 	@Override
 	public List<Path> newGeneration(int clientID, List<Path> currentGeneration) throws RemoteException {
-		return null;
+		// tutaj klient zglasza wynik swojej pracy a w zamian otrzymuje
+		// populacje do kolejnych obliczen
+		clientResults.add(currentGeneration);
+		// poinformuj watek zarzadzajacy
+		synchronized (notifier) {
+			notifier.notify();
+		}
+		// czekaj
+		try {
+			synchronized (lock) {
+				lock.wait();
+			}
+			return currentGeneration;
+		} catch (InterruptedException e) {
+			return null;
+		}
+	}
+
+	class Session extends Thread {
+		public void run() {
+			try {
+				int handledCount = 0;
+				while (true) {
+					synchronized (notifier) {
+						notifier.wait();
+					}
+					
+					if(clientRegistry.size()>0){
+						//zbierz aktualne wyniki od klientów
+						currentGeneration.clear();
+						int gatherCount = populationCount / clientRegistry.size();
+						while(!clientResults.isEmpty()){
+							List<Path> result = clientResults.pop();
+							currentGeneration.addAll(result.subList(0, gatherCount));
+							handledCount++;
+						}
+					}
+					if(handledCount == clientRegistry.size()){
+						//jesli od wszystkich zebrane
+						//uzupelnij liste jesli sa braki
+						assertSize(currentGeneration);
+						handledCount = 0;
+						//obudz czekajacych klientow
+						synchronized (lock) {
+							lock.notifyAll();
+						}
+					}
+					
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	public static void main(String[] args){
+	private void assertSize(List<Path> list){
+		while(list.size()<populationCount){
+			list.add(new Path(nodeNumbers.length).random());
+		}
+	}
+	
+	
+
+	public static void main(String[] args) {
 		if (System.getSecurityManager() == null) {
-		    System.setSecurityManager(new RMISecurityManager());
+			System.setSecurityManager(new RMISecurityManager());
 		}
 		try {
 			Server s = new Server(7878);
@@ -133,9 +236,6 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 			s.setGenerationsCount(1000);
 			s.setPopulationCount(10000);
 			CityMap m = readMap("graph.txt");
-			MapUtil mu = new MapUtil(m);
-			s.setNodeNumbers(mu.getNodeNumbers());
-			s.setCostArray(mu.getCostArray());
 			s.setMap(m);
 			Naming.rebind("//localhost:7878/ParalellGeneticServer", s);
 			System.out.println("Server started on port 7878");
@@ -147,45 +247,47 @@ public class Server extends UnicastRemoteObject implements ServerInterface{
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static CityMap readMap(String path) throws IOException {
-    	TreeMap<String, City> nodes = new TreeMap<String, City>();
-        String tmp; String [] line; int fileLine=1;
+		TreeMap<String, City> nodes = new TreeMap<String, City>();
+		String tmp;
+		String[] line;
+		int fileLine = 1;
 
-        BufferedReader in = new BufferedReader(new FileReader(new File(path)));
-        //sprawdzam czy jest nagłówek Nodes:
-        if(!(tmp = in.readLine()).equals("Nodes:")){
-            throw new FileFormatException(1, "Brak naglowka \"Nodes:\"");
-        }
+		BufferedReader in = new BufferedReader(new FileReader(new File(path)));
+		// sprawdzam czy jest nagłówek Nodes:
+		if (!(tmp = in.readLine()).equals("Nodes:")) {
+			throw new FileFormatException(1, "Brak naglowka \"Nodes:\"");
+		}
 
-        //wczytuję wierzchołki grafu
-        while((tmp = in.readLine())!=null && !tmp.equals("Edges:") ){
-            nodes.put(tmp, new City(tmp));
-            fileLine++;
-        }
+		// wczytuję wierzchołki grafu
+		while ((tmp = in.readLine()) != null && !tmp.equals("Edges:")) {
+			nodes.put(tmp, new City(tmp));
+			fileLine++;
+		}
 
-        //sprawdzam czy jest nagłówek Edges:
-        if(tmp==null){
-            throw new FileFormatException(fileLine, "Brak naglowka \"Edges:\"");
-        }else{
-            //wczytuję połaczenia między wierzchołkami
-            while((tmp = in.readLine()) != null){
-                ++fileLine;
-                line=tmp.split(" ");
-                if(line.length!=4)
-                    throw new FileFormatException(fileLine, "Bledna ilosc danych. Wymagane 4.");
-                nodes.get(line[0]).AddOutgoingRoute(nodes.get(line[1]), Integer.parseInt(line[2]),Integer.parseInt(line[3]));
-            }
-        }
-        in.close();
+		// sprawdzam czy jest nagłówek Edges:
+		if (tmp == null) {
+			throw new FileFormatException(fileLine, "Brak naglowka \"Edges:\"");
+		} else {
+			// wczytuję połaczenia między wierzchołkami
+			while ((tmp = in.readLine()) != null) {
+				++fileLine;
+				line = tmp.split(" ");
+				if (line.length != 4)
+					throw new FileFormatException(fileLine, "Bledna ilosc danych. Wymagane 4.");
+				nodes.get(line[0]).AddOutgoingRoute(nodes.get(line[1]), Integer.parseInt(line[2]), Integer.parseInt(line[3]));
+			}
+		}
+		in.close();
 
-        return new CityMap(nodes);
-    }
-    
-    static class FileFormatException extends IOException{
-        public FileFormatException(int line, String message){
-            super("Blad struktury pliku w linii " + line + ": " + message);
-        }
-    }
+		return new CityMap(nodes);
+	}
+
+	static class FileFormatException extends IOException {
+		public FileFormatException(int line, String message) {
+			super("Blad struktury pliku w linii " + line + ": " + message);
+		}
+	}
 
 }
